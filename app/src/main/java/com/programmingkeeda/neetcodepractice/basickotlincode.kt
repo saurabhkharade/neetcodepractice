52ce4744141406e0f7b6acbaff52972891008af1
Saurabh Kharade <saurabhkharade2@gmail.com>
2025-05-14 23:41:58 +0530

    containsDuplicate

--- app/src/main/java/com/programmingkeeda/neetcodepractice/basickotlincode.kt
+++ app/src/main/java/com/programmingkeeda/neetcodepractice/basickotlincode.kt
@@ -1,4 +1,18 @@
 package com.programmingkeeda.neetcodepractice
 
-class basickotlincode {
+fun containsDuplicate(nums: IntArray): Boolean {
+    val seen = HashSet<Int>()
+    for (num in nums) {
+        if (!seen.add(num)) {
+            return true // Duplicate found
+        }
+    }
+    return false // No duplicates
+}
+fun main() {
+    val nums1 = intArrayOf(1, 2, 3, 3)
+    println(containsDuplicate(nums1)) // Output: true
+
+    val nums2 = intArrayOf(1, 2, 3, 4)
+    println(containsDuplicate(nums2)) // Output: false
 }
